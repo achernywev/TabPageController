bb1f2260ec5abf363149ba4b42f0ecadc1f9c328
Aleksandr Chernyshev <achernywev@gmail.com>
2023-06-29 18:09:51 -0500

    Updated podspec, added additional customization for TabPageController

--- TabPageControllerExample/TabPageControllerExample/AppDelegate.swift
+++ TabPageControllerExample/TabPageControllerExample/AppDelegate.swift
@@ -23,29 +23,31 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
     
-    func createAndReturnTabPageController() -> TabPageController {
+    private typealias BasePageVC = TabPageController<TabPageCell>
+    private func createAndReturnTabPageController() -> BasePageVC {
         let controller1 = UIViewController()
         controller1.view.backgroundColor = UIColor.red
-        let item1 = TabPageItem(viewController: controller1, viewControllerTitle: "Title 1")
+        let item1 = TabPageItem(viewController: controller1, title: "Title 1")
         
         let controller2 = UIViewController()
         controller2.view.backgroundColor = UIColor.green
-        let item2 = TabPageItem(viewController: controller2, viewControllerTitle: "Title 2")
+        let item2 = TabPageItem(viewController: controller2, title: "Title 2")
         
         let controller3 = UIViewController()
         controller3.view.backgroundColor = UIColor.purple
-        let item3 = TabPageItem(viewController: controller3, viewControllerTitle: "Title 3")
+        let item3 = TabPageItem(viewController: controller3, title: "Title 3")
         
         let controller4 = UIViewController()
         controller4.view.backgroundColor = UIColor.magenta
-        let item4 = TabPageItem(viewController: controller4, viewControllerTitle: "Title 4")
+        let item4 = TabPageItem(viewController: controller4, title: "Title 4")
         
         let controller5 = UIViewController()
         controller5.view.backgroundColor = UIColor.blue
-        let item5 = TabPageItem(viewController: controller5, viewControllerTitle: "Title 5")
+        let item5 = TabPageItem(viewController: controller5, title: "Title 5")
         
-        let tabController = TabPageController(pageItems: [item1, item2, item3, item4, item5])
+        let tabController = BasePageVC()
         tabController.title = "TabPageController"
+        tabController.updatePageItems([item1, item2, item3, item4, item5])
         return tabController
     }
 }
